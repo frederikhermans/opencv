fcae4e811e39f2d70cb9e6e384ec43dfc0112ace
Frederik Hermans <frederik.ac.hermans@gmail.com>
2015-10-03 05:55:22 -0500

    Enable python2 module for Android.

--- platforms/scripts/cmake_android_arm.sh
+++ platforms/scripts/cmake_android_arm.sh
@@ -4,4 +4,4 @@ cd `dirname $0`/..
 mkdir -p build_android_arm
 cd build_android_arm
 
-cmake -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DCMAKE_TOOLCHAIN_FILE=../android/android.toolchain.cmake $@ ../..
+cmake -DPYTHON2_LIBRARIES=/home/kivy/python-for-android-armeabi-v7a/build/python/Python-2.7.2 -DPYTHON2_EXECUTABLE=/usr/bin/python -DPYTHON2_INCLUDE_DIR=/usr/include/python2.7 -DPYTHON2_NUMPY_INCLUDE_DIRS=/usr/lib/python2.7/dist-packages/numpy/core/include/ -DANDROID_ABI="armeabi-v7a with NEON" -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DENABLE_NEON=ON -DENABLE_VFPV3=ON -DCMAKE_TOOLCHAIN_FILE=../android/android.toolchain.cmake $@ ../..
